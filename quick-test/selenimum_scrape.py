081bd3d11197f98a67f3d407934b4ea8f2f39be9
Boris Serebrov <serebrov@gmail.com>
2016-02-16 00:12:39 +0200

    Selenium scrape test

---
+++ quick-test/selenimum_scrape.py
@@ -0,0 +1,41 @@
+# See http://stackoverflow.com/questions/35382258/google-chrome-with-selenium/35420019
+# Download the chromedriver:
+# $ wget wget http://chromedriver.storage.googleapis.com/2.21/chromedriver_linux64.zip
+# $ unzip chromedriver_linux64.zip
+#
+# Run the script as
+# $ PATH=$PATH:. python selenimum_scrape.py
+
+
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+def init_driver():
+    driver = webdriver.Chrome()
+    driver.wait = WebDriverWait(driver, 5)
+    return driver
+
+def lookup(driver, query):
+    driver.get("http://www.google.com")
+    try:
+        box = driver.wait.until(EC.presence_of_element_located(
+            (By.NAME, "q")))
+        # once we type the query, this button disappears
+        # button = driver.wait.until(EC.element_to_be_clickable(
+        #     (By.NAME, "btnK")))
+        box.send_keys(query)
+        button = driver.wait.until(EC.element_to_be_clickable(
+            (By.NAME, "btnG")))
+        button.click()
+    except TimeoutException:
+        print("Box or Button not found in google.com")
+
+if __name__ == "__main__":
+    driver = init_driver()
+    lookup(driver, "Selenium")
+    time.sleep(5)
+    driver.quit()
