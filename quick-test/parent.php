0d6a99979651a118d0619f1249e925c4015edb54
Boris Serebrov <serebrov@gmail.com>
2016-02-18 00:30:38 +0200

    Add parent singelton test

---
+++ quick-test/parent.php
@@ -0,0 +1,107 @@
+<?php
+class Application
+{
+    //Class Objects
+    public $Cookie = NULL;
+    public $Session = NULL;
+    public $TestVariable = "Hello World";
+
+    //Object initialization. Singleton design.
+    protected function __construct()
+    {
+        //Initialize cookie object.
+        $this->Cookie = Cookie::getCookie();
+    }
+
+    protected function init() {
+        //Initialize session object.
+        $this->Session = Session::getSession();
+    }
+
+    //Returns the singleton instance of The Curator class. Singleton design.
+    public static function Initialize()
+    {
+        static $instance = NULL;
+
+        if($instance === NULL)
+        {
+            $instance = new static();
+            $instance->init();
+        }
+
+        return $instance;
+    }
+
+   //Singleton design.
+   private function __clone() {}
+
+   //Singleton design.
+   private function __wakeup() {}
+}
+
+class Cookie
+{
+    //Object initalization. Singleton design.
+    protected function __construct()
+    {
+    }
+
+    //Singleton design.
+    private function __clone() {}
+
+    //Singleton design.
+    private function __wakeup() {}
+
+    //Returns the singleton instance of the cookie class. Singleton design.
+    public static function getCookie()
+    {
+        static $cookieInstance = NULL;
+
+        if($cookieInstance === NULL)
+        {
+            $cookieInstance = new static();
+        }
+
+        return $cookieInstance;
+    }
+
+    //Removes all cookies.
+    public function destroyCookies()
+    {
+        //CODE HERE
+    }
+}
+
+class Session extends Application
+{
+    //Object initialization. Singleton design.
+    protected function __construct()
+    {
+        parent::__construct();
+        //This FAILS!
+        $this->Cookie->destroyCookies();
+        //This WORKS!
+        echo $this->TestVariable;
+    }
+
+    //Singleton design.
+    private function __clone() {}
+
+    //Singleton design.
+    private function __wakeup() {}
+
+    //Returns the singleton instance of the session class. Singleton design.
+    public static function getSession()
+    {
+        static $sessionInstance = NULL;
+
+        if($sessionInstance === NULL)
+        {
+            $sessionInstance = new static();
+        }
+
+        return $sessionInstance;
+    }
+}
+
+Session::getSession();
