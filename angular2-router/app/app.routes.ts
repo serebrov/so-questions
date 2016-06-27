3ffa0df60d577b36152e04892c80998201b09ef1
Boris Serebrov <serebrov@gmail.com>
2016-06-27 23:22:01 +0300

    Remove commented routes

--- angular2-router/app/app.routes.ts
+++ angular2-router/app/app.routes.ts
@@ -4,8 +4,6 @@ import { HomeComponent } from './home.component';
 import { AboutComponent } from './about.component';
 
 export const routes: RouterConfig = [
-  //{ path: '', component: HomeComponent },
-  //{ path: 'about', component: AboutComponent }
   { path: '',                  component: HomeComponent },
   { path: ':country',          component: HomeComponent },
   { path: ':country/home',     component: HomeComponent },
