601dee8cb6f6b1af817d2003d43f70fa2dd682ee
Boris Serebrov <serebrov@gmail.com>
2018-10-29 15:34:27 +0200

    Fix formatting

--- nodejs-typescript/client/client.ts
+++ nodejs-typescript/client/client.ts
@@ -1,18 +1,15 @@
 class Person {
-
-  constructor(name:string)
-	{
-		this.name=name;
-	}
-	name: string;
+  constructor(name:string) {
+      this.name=name;
+  }
+  name: string;
 }
 
-function greeter (person:Person){
-	return "hallo "+person.name;
+function greeter (person:Person){ 
+    return "hallo "+person.name;
 }
 
 var person=new Person("bert");
-
 
 $(document).ready(function(){
     var message = greeter(person);
